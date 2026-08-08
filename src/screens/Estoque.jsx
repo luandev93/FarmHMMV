e805327c1e943c3e0d3565cc1b0a3464e0f133c3
luandev93 <luandev93@gmail.com>
2026-08-08 08:04:55 -0300

    Remove BotaoFiltros import from Estoque.jsx

--- src/screens/Estoque.jsx
+++ src/screens/Estoque.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from 'react'
-import { BotaoFiltros, BotaoOlho, Icone, Painel, Vazio, ocultar, useValores } from '../components/ui'
+import { BotaoOlho, Icone, Painel, Vazio, ocultar, useValores } from '../components/ui'
 import { useDados } from '../lib/store'
 import { movimentosRecentes, itensComMovimento } from '../lib/db'
 import { useAuth } from '../lib/auth'
